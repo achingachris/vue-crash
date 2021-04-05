c3f0058b3fdbb6189a6811343abb7dc223b22163
ChrisAchinga <achinga.chris@gmail.com>
2021-04-05 13:39:26 +0300

    page layout

--- components/Body.js
+++ components/Body.js
@@ -1,10 +1,45 @@
-import React from 'react'
+import { Container, Col, Row, Card, ListGroup, Button } from 'react-bootstrap'
 
 const Body = () => {
   return (
-    <div>
-      Body
-    </div>
+    <Container className='mt-4'>
+      <Row>
+        <Col md={8}>
+          <Card>
+            <Card.Img variant='top' src='holder.js/100px180?text=Image cap' />
+            <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </Card.Text>
+            </Card.Body>
+            <ListGroup className='list-group-flush'>
+              <ListGroup.Item>Cras justo odio</ListGroup.Item>
+              <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+              <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+            </ListGroup>
+            <Card.Body>
+              <Card.Link href='#'>Card Link</Card.Link>
+              <Card.Link href='#'>Another Link</Card.Link>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col md={4}>
+          <Card>
+            <Card.Img variant='top' src='holder.js/100px180' />
+            <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </Card.Text>
+              <Button variant='primary'>Go somewhere</Button>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
