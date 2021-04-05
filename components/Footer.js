437a2e6f0ac4fb2bef30eebc285adc4bbec28bb6
ChrisAchinga <achinga.chris@gmail.com>
2021-04-05 13:39:09 +0300

    updated footer and content

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
-    <footer>
-      <p>Footer</p>
+    <footer className='align-content-center align-items-center text-center mt-4'>
+      <p className='text-black-50'>Fundraiser &copy; 2021</p>
     </footer>
   )
 }
