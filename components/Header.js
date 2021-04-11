26be1426df273cc8197597754fc5dbb61b6cc6fb
ChrisAchinga <achinga.chris@gmail.com>
2021-04-11 23:11:09 +0300

    clean

--- components/Header.js
+++
@@ -1,9 +0,0 @@
-const Header = () => {
-  return (
-    <header className='text-center align-content-center align-items-center mt-5'>
-      <h1>Fund Raiser</h1>
-    </header>
-  )
-}
-
-export default Header
--- pages/_app.js
+++
@@ -1,9 +0,0 @@
-// add bootstrap css 
-import 'bootstrap/dist/css/bootstrap.css'
-import '../styles/globals.css'
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
-
-export default MyApp
