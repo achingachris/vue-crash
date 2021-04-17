75b242fc3651706ec83cb4bc1379b52d49c795eb
ChrisAchinga <achinga.chris@gmail.com>
2021-04-17 18:40:00 +0300

    /

--- pages/_app.js
+++
@@ -1,8 +0,0 @@
-import '@styles/globals.css'
-import 'tailwindcss/tailwind.css'
-
-function Application({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
-
-export default Application
